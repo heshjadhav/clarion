fd251fa0a625ac59f9904f283ebde91c3d3d4181
mahesh jadhav <mjmjadhav@gmail.com>
2021-11-10 01:00:31 +0530

    Create task.js
    new task

---
+++ task.js
@@ -0,0 +1,80 @@
+const users = [
+    {
+      name: "yagnesh",
+      age: 30,
+      gender: "male",
+    },
+    {
+      name: "virat",
+      age: 28,
+      gender: "male",
+    },
+    {
+      name: "rohit",
+      age: 32,
+      gender: "male",
+    },
+    {
+      name: "dipika",
+      age: 34,
+      gender: "female",
+    },
+    {
+      name: "alia",
+      age: 19,
+      gender: "female",
+    },
+    {
+      name: "rekha",
+      age: 64,
+      gender: "female",
+    },
+    {
+      name: "Taimur",
+      age: 18,
+      gender: "male",
+    },
+  ];
+
+  //console.log(users);
+
+  
+// some using reduce
+const isRekhaExists = users.reduce((acc,curr) => {
+    if(curr.name == 'rekha'){
+        acc = curr.name;
+    }
+    return !!acc;
+},false);
+
+console.log(isRekhaExists);
+
+
+const isEveryoneAdult = users.reduce((acc,curr) => {
+    acc = false;
+    if(curr.age >= 18){
+        acc = true;
+    }
+    return !!acc;
+},false);
+  
+console.log(isEveryoneAdult);
+
+const findData = users.reduce((acc,curr) => {
+    if('alia' === curr.name){
+        acc = curr;
+    }
+    return acc;
+},{});
+
+console.log(findData);
+
+updateGender = users.reduce((acc,curr)=>{
+    if('female' === curr.gender){
+        curr.gender = 'male';
+    }
+    acc.push(curr);
+    return acc;
+},[]);
+
+console.log(updateGender);
